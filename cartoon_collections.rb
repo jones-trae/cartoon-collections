10ce547e586f0bbfbe32bbc374b498a32c6f8be8
Valton Jones <jones.trae@gmail.com>
2021-03-03 16:49:19 -0800

    commit tree 1bd51bad9e02a8a03113a37ebaf8740cbc00bb27

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  # Use `each_with_index` to enumerate over the provided array
+  #
+  # Print a numbered list of each element
+  dwarves.each_with_index do |element, index|
+    puts "#{index + 1}. #{element}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  new_elements = elements.map(&:capitalize) #updating array to capitalize each word
+  new_elements.map do |word|
+    word + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  calls_long.any? do |word|
+    word.length > 4
+  end# code an argument here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find{|name| cheese_types.include?(name)}# code an argument here
 end
